74fad82dcb09456f075866da2de17d2ecd9c21fc
Aswin110 <aswinashok110@gmail.com>
2023-09-28 19:36:04 +0530

    javascript side completed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 const sliders = document.querySelectorAll('.slide');
-const dots = document.querySelectorAll('.dots');
+const dots = document.querySelectorAll('.dot');
 const prev =document.querySelector('.prev');
 const next =document.querySelector('.next');
 
-console.log('hi');
 let initialValue = 1;
-change(0);
+change(-1);
 
 function change(n){
 	initialValue += n;
+	if(initialValue > sliders.length - 1){
+		initialValue = 0;
+	}
+	if(initialValue < 0 ){
+		initialValue = sliders.length - 1;
+	}  
+
 	for (let i=0; i<sliders.length; i++){
 		sliders[i].style.display = 'none';
 	}
+	for(let i=0; i<dots.length; i++){
+		dots[i].classList.remove('active');
+	}
 	sliders[initialValue].style.display = 'block';
-	dots[initialValue].className += 'active';
+	dots[initialValue].classList.add('active');
 }
 
-prev.addEventListener('click', change(-1));
-next.addEventListener('click', change(1));
+prev.onclick = function(){change(-1);};
+next.onclick = function(){change(1);};
+dots.forEach(dot =>{
+	dot.addEventListener('click', function(){
+		initialValue = parseInt(this.id);
+		changeDot();
+	});
+});
+
+function changeDot(){
+	console.log('inside changeDot');
+
+	for (let i=0; i<sliders.length; i++){
+		sliders[i].style.display = 'none';
+	}
+	for(let i=0; i<dots.length; i++){
+		dots[i].classList.remove('active');
+	}
+	sliders[initialValue].style.display = 'block';
+	dots[initialValue].classList.add('active');
+}
